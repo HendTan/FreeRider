945f6617e7f2a43240f76f82fc09a376f3b64d29
HendTan <51568773+HendTan@users.noreply.github.com>
2025-03-19 19:06:07 +0800

    Deploy to GIT

---
+++ utils/js/createProduct.js
@@ -0,0 +1,12 @@
+﻿$(function () {
+    $("#imgBtn").on("click", function (e) {
+        e.preventDefault();
+        document.getElementById("ImgInput").click();
+    })
+
+    $("#ImgInput").on("change", function () {
+        let file = $("input[type=file]").get(0).files[0];
+        console.log(file);
+        document.getElementById("imgPreview").src = URL.createObjectURL(file);
+    })
+})
